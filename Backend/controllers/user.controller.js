61e3f9310ff67338632defe063f90881ab478ae2
ankit1657 <ankit.ak95744@gmail.com>
2025-01-04 15:40:32 +0530

    user registration done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
-const blackListTokenModel = require('../models/blackListToken.model');
+// const blackListTokenModel = require('../models/blackListToken.model');
 
 module.exports.registerUser = async (req, res, next) => {
 
@@ -34,31 +34,31 @@ module.exports.registerUser = async (req, res, next) => {
 
 }
 
-module.exports.loginUser = async (req, res, next) => {
+// module.exports.loginUser = async (req, res, next) => {
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(400).json({ errors: errors.array() });
+//     }
 
-    const { email, password } = req.body;
+//     const { email, password } = req.body;
 
-    const user = await userModel.findOne({ email }).select('+password');
+//     const user = await userModel.findOne({ email }).select('+password');
 
-    if (!user) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-    }
+//     if (!user) {
+//         return res.status(401).json({ message: 'Invalid email or password' });
+//     }
 
-    const isMatch = await user.comparePassword(password);
+//     const isMatch = await user.comparePassword(password);
 
-    if (!isMatch) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-    }
+//     if (!isMatch) {
+//         return res.status(401).json({ message: 'Invalid email or password' });
+//     }
 
-    const token = user.generateAuthToken();
+//     const token = user.generateAuthToken();
 
-    res.cookie('token', token);
+//     res.cookie('token', token);
 
-    res.status(200).json({ token, user });
-}
+//     res.status(200).json({ token, user });
+// }
 
